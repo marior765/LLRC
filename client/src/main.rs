553087839483cd3caa28139fea069caecd274e98
marior765 <d.bohatyrchk@gmail.com>
2019-05-17 22:51:52 +0300

    fix moved value

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,6 +1,5 @@
 use server::*;
-use std::thread;
 
 fn main() {
-    init_client("127.0.0.1:6142");
+    init_client("127.0.0.1:6000");
 }
--- server/src/lib.rs
+++ server/src/lib.rs
@@ -10,6 +10,7 @@ use std::net::{
 };
 use std::sync::mpsc::{self, TryRecvError};
 use std::time::Duration;
+use futures::Async;
 
 const MSG_SIZE: usize = 30;
 
@@ -28,16 +29,16 @@ pub fn init_server(addr: &str) {
     let mut server = TcpListener::from_std(server, &handle).unwrap(); 
     // .and_then(|s| {
         'server: loop {
-            if let Ok((mut socket, addr)) = server.accept() {
+            if let Ok(Async::Ready((socket, addr))) = server.poll_accept() {
                 println!("Client {} connected", addr);
 
                 let transmitter = transmitter.clone();
                 clients.push(socket.try_clone().expect("failed to clone"));
 
                 thread::spawn(move || loop {
-                    let mut buff = vec![0; MSG_SIZE];
+                    let buff = vec![0; MSG_SIZE];
 
-                    read_exact(socket, &mut buff);
+                    // read_exact(socket, &mut buff);
                     let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
                     let msg = String::from_utf8(msg).expect("failed to convert utf8");
 
@@ -59,48 +60,21 @@ pub fn init_server(addr: &str) {
 
             sleep();
         }
-    // })
-    // .map_err(|e| {
-    //     println!("Error: {}", e);
-    // });
-    // let addr = addr.parse().unwrap();
-    // let listener = TcpListener::bind(&addr).unwrap();
-
-    // let server = listener.incoming()
-    //     .map_err(|e| println!("Error: {}", e))
-    //     .for_each(|s| {
-    //         let (reader, writer) = s.split();
-
-    //         let copied_bytes = copy(reader, writer);
-
-    //         let msg = copied_bytes.then(move |result| {
-    //             match result {
-    //                 Ok((amt, _, _)) => println!("wrote {} bytes", amt),
-    //                 Err(e) => println!("error: {}", e),
-    //             }
-
-    //             Ok(())
-    //         });
-
-    //         tokio::spawn(msg)
-    //     });
-
-    //     tokio::run(server);
 }
 
 pub fn init_client(addr: &str) {
     // let addr = addr.parse().unwrap();
     let mut client = StdTcpStream::connect(addr).expect("Stream failed to connect");
-    client.set_nonblocking(true).expect("failed to initiate non-blocking");
+    &client.set_nonblocking(true).expect("failed to initiate non-blocking");
 
     let handle = tokio::reactor::Handle::default();
 
     let (transmitter, receiver) = mpsc::channel::<String>();
 
-    let client = TcpStream::from_std(client, &handle).and_then(|stream| {
-        thread::spawn(move || loop {
+    TcpStream::from_std(client.try_clone().unwrap(), &handle).and_then(|stream| {
+        // thread::spawn(move || loop {
             let mut buff = vec![0; MSG_SIZE];
-            read_exact(stream, buff);
+            read_exact(stream, &mut buff);
             let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
             println!("message received: {:?}", {});
 
@@ -112,11 +86,11 @@ pub fn init_client(addr: &str) {
                     println!("message sent {:?}", msg);
                 }
                 Err(TryRecvError::Empty) => (),
-                Err(TryRecvError::Disconnected) => break
+                Err(TryRecvError::Disconnected) => ()
             }
 
             thread::sleep(Duration::from_millis(100));
-        });
+        // });
 
         println!("write a Message:");
         loop {
@@ -127,22 +101,6 @@ pub fn init_client(addr: &str) {
         }
         Ok(())
     });
-
-    // let client = TcpStream::connect(&addr).and_then(|stream| {
-    //     println!("created stream");
-    //     let mut input = String::new();
-    //     std::io::stdin().read_line(&mut input).unwrap();
-    //     write_all(stream, input).then(|result| {
-    //         println!("wrote to stream; success={:?}", result.is_ok());
-    //         Ok(())
-    //     })
-    // })
-    // .map_err(|err| {
-    //     println!("connection error = {:?}", err);
-    // });
-    // println!("About to create the stream and write to it...");
-    // tokio::run(client);
-    // println!("Stream has been created and written to.");
 }
 
 pub fn init_connection(addr: &'static str) {
